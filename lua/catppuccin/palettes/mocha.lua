159fe37f69a2025790e77dff4697c48c610b66a5
Zafar Khodjaev <zaffenergizante@gmail.com>
2024-07-16 01:30:48 +0500

    add oxocarbon

--- lua/catppuccin/palettes/mocha.lua
+++ lua/catppuccin/palettes/mocha.lua
@@ -1,28 +1,48 @@
 return {
-	rosewater = "#f5e0dc",
-	flamingo = "#f2cdcd",
-	pink = "#f5c2e7",
-	mauve = "#cba6f7",
-	red = "#f38ba8",
-	maroon = "#eba0ac",
-	peach = "#fab387",
-	yellow = "#f9e2af",
-	green = "#a6e3a1",
-	teal = "#94e2d5",
-	sky = "#89dceb",
-	sapphire = "#74c7ec",
-	blue = "#89b4fa",
-	lavender = "#b4befe",
-	text = "#cdd6f4",
-	subtext1 = "#bac2de",
-	subtext0 = "#a6adc8",
-	overlay2 = "#9399b2",
-	overlay1 = "#7f849c",
-	overlay0 = "#6c7086",
-	surface2 = "#585b70",
-	surface1 = "#45475a",
-	surface0 = "#313244",
-	base = "#1e1e2e",
-	mantle = "#181825",
-	crust = "#11111b",
+	rosewater = "#ffffff",
+	flamingo = "#82cfff",
+	pink = "#f5c2e7", -- not base16
+	mauve = "#be95ff",
+	red = "#3ddbd9",
+	maroon = "#eba0ac", -- not base16
+	peach = "#78a9ff",
+	yellow = "#ee5396",
+	green = "#33b1ff",
+	teal = "#ff7eb6",
+	sky = "#89dceb",      -- not base16
+	sapphire = "#74c7ec", -- not base16
+	blue = "#42be65",
+	lavender = "#08bdba",
+	text = "#f2f4f8",
+	subtext1 = "#bac2de", -- not base16
+	subtext0 = "#a6adc8", -- not base16
+	overlay2 = "#9399b2", -- not base16
+	overlay1 = "#7f849c", -- not base16
+	overlay0 = "#6c7086", -- not base16
+	surface2 = "#dde1e6",
+	surface1 = "#525252",
+	surface0 = "#393939",
+	base = "#161616",
+	mantle = "#262626",
+	crust = "#11111b", -- not base16
 }
+
+-- -- Dark
+-- return {
+-- 	base00 = "#161616",
+-- 	base01 = "#262626",
+-- 	base02 = "#393939",
+-- 	base03 = "#525252",
+-- 	base04 = "#dde1e6",
+-- 	base05 = "#f2f4f8",
+-- 	base06 = "#ffffff",
+-- 	base07 = "#08bdba",
+-- 	base08 = "#3ddbd9",
+-- 	base09 = "#78a9ff",
+-- 	base0A = "#ee5396",
+-- 	base0B = "#33b1ff",
+-- 	base0C = "#ff7eb6",
+-- 	base0D = "#42be65",
+-- 	base0E = "#be95ff",
+-- -- 	base0F = "#82cfff",
+-- }
---
+++ lua/catppuccin/palettes/oxocarbon.lua
@@ -0,0 +1,48 @@
+return {
+    rosewater = "#ffffff",
+    flamingo = "#82cfff",
+    pink = "#f5c2e7", -- not base16
+    mauve = "#be95ff",
+    red = "#3ddbd9",
+    maroon = "#eba0ac", -- not base16
+    peach = "#78a9ff",
+    yellow = "#ee5396",
+    green = "#33b1ff",
+    teal = "#ff7eb6",
+    sky = "#89dceb",      -- not base16
+    sapphire = "#74c7ec", -- not base16
+    blue = "#42be65",
+    lavender = "#08bdba",
+    text = "#f2f4f8",
+    subtext1 = "#bac2de", -- not base16
+    subtext0 = "#a6adc8", -- not base16
+    overlay2 = "#9399b2", -- not base16
+    overlay1 = "#7f849c", -- not base16
+    overlay0 = "#6c7086", -- not base16
+    surface2 = "#dde1e6",
+    surface1 = "#525252",
+    surface0 = "#393939",
+    base = "#161616",
+    mantle = "#262626",
+    crust = "#11111b", -- not base16
+}
+
+-- Light
+-- return {
+-- 	base00 = "#f2f4f8",
+-- 	base01 = "#dde1e6",
+-- 	base02 = "#525252",
+-- 	base03 = "#161616",
+-- 	base04 = "#262626",
+-- 	base05 = "#393939",
+-- 	base06 = "#525252",
+-- 	base07 = "#08bdba",
+-- 	base08 = "#ff7eb6",
+-- 	base09 = "#ee5396",
+-- 	base0A = "#FF6F00",
+-- 	base0B = "#0f62fe",
+-- 	base0C = "#673AB7",
+-- 	base0D = "#42be65",
+-- 	base0E = "#be95ff",
+-- 	base0F = "#37474F",
+-- }
