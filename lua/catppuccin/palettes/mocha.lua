fe656aba34fba3efb57049fd4960b51d0761af60
Zafar Khodjaev <zaffenergizante@gmail.com>
2024-07-16 02:02:29 +0500

    Revert "updat eoxocarbon"
    This reverts commit 842989d534fec5a0849846412894e922983b26c9.

--- lua/catppuccin/palettes/mocha.lua
+++ lua/catppuccin/palettes/mocha.lua
@@ -1,33 +1,33 @@
 return {
-	rosewater = "#f2f4f8",
-	flamingo = "#f2cdcd",
-	pink = "#ff7eb6",
+	rosewater = "#ffffff",
+	flamingo = "#82cfff",
+	pink = "#f5c2e7", -- not base16
 	mauve = "#be95ff",
-	red = "#ee5396",
-	maroon = "#eba0ac",
-	peach = "#ff6f00",
-	yellow = "#ffab91",
-	green = "#42be65",
-	teal = "#08bdba",
-	sky = "#3ddbd9",
-	sapphire = "#78a9ff",
-	blue = "#33b1ff",
-	lavender = "#0f62fe",
+	red = "#3ddbd9",
+	maroon = "#eba0ac", -- not base16
+	peach = "#78a9ff",
+	yellow = "#ee5396",
+	green = "#33b1ff",
+	teal = "#ff7eb6",
+	sky = "#89dceb",      -- not base16
+	sapphire = "#74c7ec", -- not base16
+	blue = "#42be65",
+	lavender = "#08bdba",
 	text = "#f2f4f8",
-	subtext1 = "#d2d9e7",
-	subtext0 = "#b2bed6",
-	overlay2 = "#96A4AE",
-	overlay1 = "#9badb6",
-	overlay0 = "#37474F",
+	subtext1 = "#bac2de", -- not base16
+	subtext0 = "#a6adc8", -- not base16
+	overlay2 = "#9399b2", -- not base16
+	overlay1 = "#7f849c", -- not base16
+	overlay0 = "#6c7086", -- not base16
 	surface2 = "#dde1e6",
 	surface1 = "#525252",
 	surface0 = "#393939",
 	base = "#161616",
 	mantle = "#262626",
-	crust = "#131313",
+	crust = "#11111b", -- not base16
 }
 
--- Dark
+-- -- Dark
 -- return {
 -- 	base00 = "#161616",
 -- 	base01 = "#262626",
